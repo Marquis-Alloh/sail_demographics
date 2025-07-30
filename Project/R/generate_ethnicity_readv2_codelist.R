3e80782c3d0cd8a8062753274d7c45e8ecb2f978
Marquis Alloh <marquis.alloh@hdruk.ac.uk>
2025-07-30 12:41:57 +0100

    changes made to the cleaning process as well as code to save to created datafile, also deleted the unneeded root directory

--- Project/R/generate_ethnicity_readv2_codelist.R
+++ Project/R/generate_ethnicity_readv2_codelist.R
@@ -4,45 +4,67 @@ library(dplyr)
 library(readxl)
 library(here)
 
-setwd("C:\Users\MarquisAlloh\Documents\code\task_2\Read_v2\Project")
 glue('we are now here: {getwd()}')
 
 #file path
-ctv3_to_readv2_map <- read_tsv(here("codelists","raw","ctv3rctmap_uk_20200401000002.txt"))
-ethnicity_ctv3_codes  <- read_csv(here("codelists","raw","opensafely-ethnicity-2020-04-27.csv"))
-readv2_lookup  <- read_excel(here("codelists","raw","read code V2 - codes and descriptions.xlsx"))
+ctv3_to_readv2_map <- read_csv(here("Project","codelists","raw","ctv3rctmap_uk_20200401000002.csv.xz"))
+ethnicity_ctv3_codes  <- read_csv(here("Project","codelists","raw","opensafely-ethnicity-2020-04-27.csv"))
+readv2_lookup  <- read_csv(here("Project","codelists","raw","readv2_codes_descriptions.csv.xz"))
 
-#setting up our tables for the join
-ethnicity_ctv3_codes  <- ethnicity_ctv3_codes  |>
-  rename(ctv3_conceptid = Code,
-         ethnicity_17 = Grouping_16,
-         ethnicity_5 = Grouping_6,
-         ethnicity_17_Description = Description)
+# # Standardise column names to snake_case and rename key columns for clarity
+ethnicity_ctv3_codes <- ethnicity_ctv3_codes |>
+  janitor::clean_names() |>
+  rename(
+    ctv3_conceptid = code,
+    ctv3_description = description,
+    ethnicity_16 = grouping_16,
+    ethnicity_5 = grouping_6,
+  )
 
 ctv3_to_readv2_map <- ctv3_to_readv2_map |>
-  rename(ctv3_conceptid = CTV3_CONCEPTID,
-         ctv3_termid = CTV3_TERMID,
-         v2_conceptid = V2_CONCEPTID,
-         v2_termid = V2_TERMID)
+  janitor::clean_names()
 
-readv2_lookup  <- readv2_lookup  |>
-  rename(v2_conceptid = ReadCode,
-         v2_termid = TermCode,
-         ethnicity_5_Description = Description)
+readv2_lookup <- readv2_lookup |> 
+  janitor::clean_names() |>
+  rename(
+    v2_conceptid = read_code,
+    v2_termid = term_code,
+    v2_description = description
+  )
 
-# The Joins are occurring here
-semi_joined_df <- left_join(ethnicity_ctv3_codes , ctv3_to_readv2_map, by = "ctv3_conceptid", relationship = "many-to-many")
+# Join CTV3 ethnicity codes with mapping table
+ctv3_mapped <- ethnicity_ctv3_codes |>
+  left_join(
+    ctv3_to_readv2_map, 
+    by = "ctv3_conceptid", 
+    relationship = "many-to-many"
+  )
 
-#c() is used as it must be joined based on multiple columns, this prevents the many-to-many issue from happening
-Fully_joined_df <- left_join(semi_joined_df, readv2_lookup , by = c("v2_termid" = "v2_termid", "v2_conceptid" = "v2_conceptid") , relationship = "many-to-many")
+# Join with Read V2 lookup table
+ethnicity_read_v2 <- ctv3_mapped |>
+  left_join(
+    readv2_lookup, 
+    by = c("v2_conceptid", "v2_termid"), 
+    relationship = "many-to-many"
+  )
 
-Grimy_df <- Fully_joined_df
+# Remove duplicates based on Read V2 code and term
+ethnicity_read_v2 <- ethnicity_read_v2 |> 
+  distinct(v2_conceptid, v2_termid, .keep_all = TRUE)
 
-# As far as I'm aware the other columns aren't of interest 
-ethnicity_read_v2 <- Grimy_df |>
-  select(ctv3_conceptid,ctv3_termid, v2_conceptid, v2_termid, ethnicity_5, ethnicity_17, ethnicity_5_Description, ethnicity_17_Description) |>
-  distinct(v2_conceptid, v2_termid, .keep_all = TRUE) |> # went from 354 obs to 267 obs
-  drop_na(c(v2_conceptid,v2_termid,ctv3_conceptid,ctv3_termid)) # went from 267 obs to 253 obs
+# Drop rows with missing key identifiers
+ethnicity_read_v2 <- ethnicity_read_v2 |> 
+  drop_na(v2_conceptid, v2_termid)
+
+# Final cleaned and deduplicated Read v2 codelist
+ethnicity_read_v2 <- ethnicity_read_v2 |> 
+  select(
+    code = v2_conceptid,
+    term = v2_termid,
+    description = v2_description,
+    ethnicity_5,
+    ethnicity_16,
+  )
 
 
-write.csv(ethnicity_read_v2, here("codelists","raw","ethnicity_readv2.csv"))
+write.csv(ethnicity_read_v2, here("Project","codelists","ethnicity_readv2.csv"))
